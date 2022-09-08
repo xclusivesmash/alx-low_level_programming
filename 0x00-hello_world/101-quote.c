e2cca317961ce0396190ab5dd090eea5d557e9e9
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-08 14:19:02 -0700

    printing without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(s, stdout);
+	return (1);
+}
