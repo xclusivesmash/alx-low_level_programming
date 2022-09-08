5095601c36f5d10be9da2a2dae9b50a9fbb0560e
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-08 13:07:37 -0700

    C program featuring puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* using puts() */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
