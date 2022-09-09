2caf3479bec397c198ef67bffcaa130bbe0ff429
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-09 14:08:34 -0700

    printing digits to console

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
