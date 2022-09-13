971ea097586b2c6a0d0b05db6b597ba8e544c2dc
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-13 16:44:40 -0700

    print alphabets 10x on each line of stdout

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets (x10) on each line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 1;
+	
+	while (i <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		c = 'a';
+		i++;
+		_putchar('\n');
+	}
+}
