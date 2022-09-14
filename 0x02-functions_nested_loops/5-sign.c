18aaf9aa66f1be222a8f449733404f782b7f8c79
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-13 17:31:00 -0700

    performs sign check on a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: input to be checked
+ * Return: 1 if positive, -1 if negative, 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else if (n > 0) 
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
