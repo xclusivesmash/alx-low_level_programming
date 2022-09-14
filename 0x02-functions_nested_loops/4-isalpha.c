4895155e23e366e639feabbe2cda0a708d66d609
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-13 17:15:34 -0700

    check if an input is alphabetic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if an input is alphabetic or not
+ * @c: input to be checked
+ * Return: 1 if is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
