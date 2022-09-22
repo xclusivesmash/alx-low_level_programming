c2fbcf7d07e0106393ecfb90ddbdea2e78826992
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-22 15:30:55 -0700

    string comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,7 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int check;
 
+	/* test if the strings have reached '\0' */
 	i = 0;
 	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
 	{
@@ -16,6 +17,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			check = *(s1 + i) - *(s2 + i);
 			return (check);
+		}
 		i++;
 	}
 	return (0);
