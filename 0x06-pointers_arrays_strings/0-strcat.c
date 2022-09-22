6704630ccb8480cf0dce2bd88ef36b63c5c0cb1c
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-22 14:17:11 -0700

    string concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,22 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 
 	i = 0;
-	while (*dest != '\0')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 
 	/* set i = last element of dest */
-	while (*src != '\0')
+	j = 0;
+	while (*(src + j) != '\0')
 	{
-		dest[i] = *src;
-		src++;
+		*(dest + i) = *(src + j);
 		i++;
+		j++;
 	}
-	dest[i] = '\0';
+	*(dest + i) = '\0';
 	return (dest);
 }
