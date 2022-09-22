b94dddc5b7e5aacf97fa4e1608bb51e0ddf071b6
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-22 16:57:06 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+/**
+ * *leet - encodes a string in 1337 leet
+ * @s: string to be encoded
+ * Return: s
+ */
+char *leet(char *s)
+{
+	/* map characters to numbers for encoding */
+	char mm[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char xx[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int i;
+	int j;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (mm[j] != '\0')
+		{
+			if (*(s + i) == mm[j])
+			{
+				*(s + i) = xx[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
