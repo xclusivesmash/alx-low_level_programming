a3b625c03f61b6a3d0d2c8c98116706bc39016bc
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-22 15:22:50 -0700

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: string to compare to
+ * @s2: string to compare against
+ * Return: 0 if true, othersiwe return check
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int check;
+
+	i = 0;
+	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
+	{
+		if (*(s1 + i) != *(s2 + i))
+		{
+			check = *(s1 + i) - *(s2 + i);
+			return (check);
+		i++;
+	}
+	return (0);
+}
