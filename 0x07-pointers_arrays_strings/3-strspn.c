b20dd1fcc846c5d4f6d3fae9a5695d35ef09b99f
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-27 15:15:04 -0700

    initial script

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+/**
+* _strspn - gets the length of a prefix substring
+* @s: main string
+* @accept: substring
+* Return: length
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int sum = 0;
+	int i = 0;
+	int j;
+
+	while (accept[i] != '\0')
+	{
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (accept[i] == s[j])
+			{
+				sum += 1;
+				break;
+			}
+			j++;
+		}
+		i++
+	}
+	return (sum + 1);
+}
