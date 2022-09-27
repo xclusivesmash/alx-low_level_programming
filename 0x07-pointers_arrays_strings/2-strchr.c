3b162a95b2b76fd050a05ea25853673fb487a8d1
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-27 14:36:22 -0700

    initial commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,17 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (*(s + i) != '\0')
+	/* first locate postion of c in s */
+	while (*(s + i) != '\0' && *(s + i) != c)
 	{
-		if (*(s + i) == c)
-		{
-			return (&(*(s + i)));
-		}
-		else
-		{
-			return (NULL);
-		}
 		i++;
+	}
+	if (*(s + i) == c)
+	{
+		return (&*(s + i));
+	}
+	else
+	{
+		return (NULL);
 	}
 }
