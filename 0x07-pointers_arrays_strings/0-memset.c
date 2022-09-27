4ba239134be291b0ff2a3d6b8ae57a6c725055d3
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-27 13:56:21 -0700

    created version of memset function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,4 @@
 /**
-* _strlen - finds the length of a string array
-* @s: passed string
-* Return: length of the string
-*/
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
 * *_memset - fills memory with a constant byte
 * @s: string array to fill on
 * @b: byte to fill in s
@@ -26,7 +9,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	if (n <= _strlen(s))
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
