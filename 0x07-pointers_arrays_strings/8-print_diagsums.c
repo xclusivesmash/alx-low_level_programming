a39e3838cca69339b4468e3259887b10486430b4
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-27 16:30:30 -0700

    diagonal sums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include <std.io>
+
+/**
+* print_diagsums - prints the sum of the two 
+* diagonals of a square matrix of integers
+* @a: square matrix
+* @size: size of matrix
+* Return: nothing
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int sum1 = 0;
+	int sum2 = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += a[i][j];
+			}
+			if (j == (size - (i + 1)))
+			{
+				sum2 += a[i][j];
+			}
+		}
+	}
+}
