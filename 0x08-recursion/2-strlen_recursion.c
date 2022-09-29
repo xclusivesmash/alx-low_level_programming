6fe03b0717d8cde8bd5aadb24eaac97aaf1fe715
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-29 16:02:24 -0700

    clear

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+* strlen_recursion - finds the length of a string
+* @s: string for which length has to be found
+* Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
