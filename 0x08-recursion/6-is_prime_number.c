3a1f79430a8fde96d22b44d8e315a1315d5267bb
xclusivesmash <sbumatshiane916@gmail.com>
2022-09-29 18:44:41 -0700

    initial formation

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+/**
+* prime_test - checks if a number is prime
+* @n: number to be checked
+* @d: divisor
+* Return: 1 if prime, 0 otherwise.
+*/
+int prime_test(int n, int d)
+{
+	if (n % d == 0)
+	{
+		return (0);
+	}
+	if (n == d)
+	{
+		return (1);
+	}
+}
+
+/**
+* is_prime_number - checks if a number is prime.
+* @n: number to be checked
+* Return: 1 if true, otherwise 0
+*/
+int is_prime_number(int n)
+{
+	int i;
+
+	i = 3;
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (prime_test(n, i));
+}
