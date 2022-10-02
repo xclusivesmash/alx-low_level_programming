7bca1ab96936e57301d73d3daa86d95dad6c970d
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-02 04:39:46 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints the name of the executable
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (on success)
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
