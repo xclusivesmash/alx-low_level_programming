62eabe5a9432dedf35e775cb38e34c92b4189bd4
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-05 09:45:48 -0700

    initial commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _strlen - computes the length of string
+* @s: pointer to char
+* Return: size of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* _strdup - returns a pointer to a newly
+* allocated space in memory
+* @str: string to be copied
+* Return: pointer
+*/
+char *_strdup(char *str)
+{
+	char *newstr;
+	int i, len;
+
+	len = _strlen(str);
+	if (str == NULL)
+		return (NULL);
+	newstr = malloc(len);
+	if (newstr == NULL)
+		return (NULL);
+	/* start copying elements of str */
+	i = 0;
+	while (i < len)
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	/* every string ends with '\0' */
+	newstr[i] = '\0';
+	return (newstr);
+}
