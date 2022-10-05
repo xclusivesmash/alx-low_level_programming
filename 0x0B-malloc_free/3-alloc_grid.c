997eab215eb61caf338ea6faa60a549ba4afb89c
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-05 11:22:04 -0700

    freed some space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,14 +12,23 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **array;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(array[i]);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
