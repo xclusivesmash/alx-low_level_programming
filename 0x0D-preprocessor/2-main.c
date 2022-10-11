9a8538bf9fe897d37c8b881d20770b6ab02f3e2c
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-11 13:47:04 -0700

    initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the file
+* the program is compiled from
+*
+* Return: Always 0 (on success)
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
