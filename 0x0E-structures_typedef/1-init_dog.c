3f0a026e1d3490c0593f1927a75241107efe90ec
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-11 15:43:38 -0700

    initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+* init_dog - initializes a struct of dog family
+* @d: address to member of struct dog
+* @name: name of dog
+* @age: age of dog
+* @owner: dog owner
+* Return: nothing
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name; /* equivalent to d->name */
+	(*d).age = age;
+	(*d).owner = owner;
+}
