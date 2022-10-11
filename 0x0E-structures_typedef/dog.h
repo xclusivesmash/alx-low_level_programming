441e23d01974765a412b91431ef35838760ce15c
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-11 15:33:44 -0700

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure for dogs
+* @name: 1st member
+* @age: 2nd member
+* @owner: 3rd member
+* Descriptions: simple structure (no
+* longer description needed.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
