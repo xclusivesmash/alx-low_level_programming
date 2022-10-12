81040a65922b5464d922eac50b8697412c910853
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-11 17:14:53 -0700

    initial commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* free_dog - frees dog
+* @d: struct belonging to struct dog
+* Return: nothing
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name)
+		free((*d).name);
+	if ((*d).owner)
+		free((*d).owner);
+	free(d);
+}
