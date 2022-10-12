8144aea1f0f6a63bd6d4bab1308c544a1cfd0412
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-11 17:01:19 -0700

    initial commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* _strcpy - copies ones string contents to another
+* @dest:base string
+* @src: string from which to copy
+* Return: pointer to copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < _strlen(src); i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+/**
+* _strlen - computes length of string
+* @s: string
+* Return: len
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+* new_dog - creates a new dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: pointer to struct dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d_new;
+	char *name_copy;
+	char *owner_copy;
+
+	d_new = malloc(sizeof(dog_t));
+
+	/*check functionality*/
+	if (d_new == NULL)
+		return (NULL);
+
+	if (name == NULL)
+		(*d_new).name = NULL;
+	else
+	{
+		/* 1 for null terminating character */
+		name_copy = malloc(_strlen(name) + 1);
+		if (name_copy == NULL)
+		{
+			free(name_copy);
+			free(d_new);
+			return (NULL);
+		}
+		(*d_new).name = _strcpy(name_copy, name);
+	}
+
+	if (owner == NULL)
+		(*d_new).owner = NULL;
+	else
+	{
+		/* 1 for null terminating character */
+		owner_copy = malloc(_strlen(owner) + 1);
+		if (owner_copy == NULL)
+		{
+			free(owner_copy);
+			free(d_new);
+			return (NULL);
+		}
+		(*d_new).owner = _strcpy(owner_copy, owner);
+	}
+
+	return (d_new);
+}
