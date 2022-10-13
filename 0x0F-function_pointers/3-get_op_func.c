e51626c9c868680b14f17e31450f949e13ae2dbe
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-12 17:37:18 -0700

    initial commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* get_op_func - matches operator with
+* correct function.
+* @s: operator given
+* Return: pointer to the correct function
+*/
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	}
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*s == (*ops[i]).op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
