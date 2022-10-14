8c26e3f06844159887f1dc5b7dd9d260834b6f86
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-13 17:25:56 -0700

    fixed loop iterator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, num;
 	va_list vlist;
 
-	if (n <= 0)
-		return;
-
-	va_start(vlist, n);
-	for (i = 1; i <= n; i++)
+	if (n > 0)
 	{
-		num = va_arg(vlist, int);
-		printf("%d", num);
-		if (separator != NULL && (i != n))
-			printf("%s", separator);
+		va_start(vlist, n);
+		for (i = 0; i < n; i++)
+		{
+			num = va_arg(vlist, int);
+			printf("%d", num);
+			if (separator != NULL && (i != (n - 1)))
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(vlist);
 	}
-	printf("\n");
-	va_end(vlist);
 }
