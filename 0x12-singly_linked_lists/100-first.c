f605dfb39aac05ee340ceef0c841152820fc8625
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-21 15:29:19 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* _print - prints some text before main func
+* Return: nothing
+*/
+
+void __attribute__ ((constructor)) _print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
