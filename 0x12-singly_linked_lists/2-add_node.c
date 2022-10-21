7a705f8ec33d50bdf8736fcaec33471bde96194e
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-21 14:03:45 -0700

    updated malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	/* malloc check */
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t *));
 	if (new == NULL)
 		return (NULL);
 
