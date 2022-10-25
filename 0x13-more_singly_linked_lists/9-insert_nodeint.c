96dd5800f86b2620eb8979434a5d5c66e423e079
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-25 15:57:10 -0700

    spelling fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/* set tmp to *head */
 	tmp = *head;
 
-	while ((i > 0) && *head != NULL)
+	while ((i >= 0) && tmp != NULL)
 	{
 		if (i == idx)
 		{
