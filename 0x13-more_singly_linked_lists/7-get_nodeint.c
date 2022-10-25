ba94207a221f8a56d8330cff9c0ab168d1778a94
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-25 00:46:49 -0700

    removed first function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
-* listint_len - determines #nodes in linked list
-* @h: linked list
-* Return: number of nodes
-*/
-unsigned int _listint_len(listint_t *h)
-{
-	unsigned int n;
-
-	n = 0;
-	while (h != NULL)
-	{
-		n = n + 1;
-		h = h->next;
-	}
-	return (n);
-}
-
-/**
 * get_nodeint_at_index - gets node at nth index
 * @head: pointer to listint_t
 * @index: integer
@@ -28,21 +10,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int ncount;
 
-	ncount = _listint_len(head);
+	ncount = 0;
 	if (head == NULL)
 	{
 		return (NULL);
 	}
 
-	/* check if index is within bounds */
-	if ((index >= 0) && (index < ncount))
+	while ((ncount > 0) && (head != NULL))
 	{
-		while ((index > 0) && (head != NULL))
-		{
-			head = head->next;
-			index = index - 1;
-		}
-		return (head);
+		head = head->next;
+		ncount = ncount + 1;
 	}
+
+	if (ncount == index)
+		return (head);
+
 	return (NULL);
 }
