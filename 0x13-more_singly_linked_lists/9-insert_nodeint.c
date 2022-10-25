014a0887f33b31f0fc73d9b79e914aad7feec32a
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-25 16:26:02 -0700

    fixed code logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,29 +22,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
 	/* fill in the new node */
 	new->n = n;
-
 	/* set tmp to *head */
 	tmp = *head;
-
 	if (idx == 0)
 	{
 		new->next = tmp;
 		tmp->next = new;
 		return (tmp);
 	}
-	while ((i > 0) && (tmp != NULL))
+	while (i < (idx - 1) && (tmp != NULL))
 	{
-		if (i == idx)
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
 		tmp = tmp->next;
 		i = i + 1;
 	}
-	return (NULL);
+	/* check if idx is in bout */
+	if (tmp == NULL)
+	{
+		free(new); /* unused in this case */
+		return (NULL);
+	}
+	new->next = tmp->next;
+	tmp->next = new;
+	return (new);
 }
