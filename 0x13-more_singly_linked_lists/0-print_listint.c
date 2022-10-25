6603f9619a58ac3c14b3b481edf464ac392ba36a
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-24 21:12:46 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* print_listint - prints elements of a linked list
+* @h: singly linked list
+* Return: number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count = count + 1;
+		printf("%d\n", h->n);
+	}
+	return (count);
+}
