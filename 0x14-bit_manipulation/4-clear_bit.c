7cc466a9fc53b32eb847a881ae4f5e12bf96df25
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-30 00:14:31 -0700

    initial commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* clear_bit - sets the value of a bit to 0
+* at a given index.
+* @n: pointer to integer
+* @index: integer
+* Return: 1 on success, otherwise -1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max_size;
+	unsigned long int mask = 1;
+
+	max_size = sizeof(*n) * 8;
+	/* check if index is within bounds */
+	if (index > max_size)
+	{
+		return (-1);
+	}
+	mask = mask << index;
+	*n = (*n) & (~mask);
+	return (1);
+}
