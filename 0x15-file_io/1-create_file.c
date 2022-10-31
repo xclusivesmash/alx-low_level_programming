75f03e028353c2bcb81221e68f580b0c803b7f77
xclusivesmash <sbumatshiane916@gmail.com>
2022-10-31 08:53:15 -0700

    chnaged file permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* open file */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	/* check if text_content is empty */
