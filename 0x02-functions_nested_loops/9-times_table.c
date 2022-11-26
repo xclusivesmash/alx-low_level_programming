be78d1b3cd105b310ff73622fbcc85de5276b5b3
Siphamandla Matshiane <sbumatshiane916@gmail.com>
2022-11-26 16:46:33 +0200

    updated: code logic

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,33 @@
 */
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
-            if (product <= 9)
-                _putchar('0' + product);
-            else
-            {
-                _putchar('0' + product / 10);
-                _putchar('0' + product % 10);
-            }
-            if (product <= 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = (i * j);
+
+			if (j == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
