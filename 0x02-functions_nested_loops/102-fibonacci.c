6650e1be2a284bca119a011af8b8504abd3bc6b5
Siphamandla Matshiane <sbumatshiane916@gmail.com>
2022-11-26 17:29:32 +0200

    initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - computes Fibonnaci's numbers
+* Return: always 0 (on success)
+*/
+int main(void)
+{
+    long int a = 1;
+    long int b = 2;
+    long int next, counter = 0;
+
+    printf("%li, %li, ", a, b);
+    while (counter <= (50 - 2))
+    {
+        next = a + b;
+        printf("%li", next);
+        a = b; b = next;
+        if (counter == (50 - 2))
+        {
+            printf("\n");
+        }
+        else
+        {
+            printf(", ");
+        }
+        counter = counter + 1;
+    }
+    return (0);
+}
