b761cb7dc4e96156befcaa9f6c51ece75db081ae
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-16 16:18:11 -0800

    Initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - prints some text
+ * Return: always 0 (on success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
