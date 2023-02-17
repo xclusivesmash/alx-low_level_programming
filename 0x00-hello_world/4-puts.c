d78552626305a39045bb10de6589e3809eef3599
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-16 16:13:45 -0800

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints some text
+ *
+ * Return: Always 0 (on success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
