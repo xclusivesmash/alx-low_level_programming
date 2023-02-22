bb47f819ae98856f6dacc876719ff8bff262489a
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-22 01:25:21 -0800

    Initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - ccomputes the absolue value
+ * @n: input parameter
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-1 * n);
+}
