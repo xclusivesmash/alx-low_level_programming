3df5a1c46cd2925386c898a8e18a92d5e7f1decd
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-22 01:54:51 -0800

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: results of addition
+ */
+int add(int a, int b)
+{
+	int results;
+
+	results = a + b;
+	return (results);
+}
