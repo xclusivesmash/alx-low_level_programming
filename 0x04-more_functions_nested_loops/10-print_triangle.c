92179b72dbd3f56b7adeb2f171f7dc1d6bd74e6d
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-24 11:53:02 -0800

    Initial commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle using #
+ * @size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+			{
+				_putchar(' ');
+			}
+			for (index = 0; index < hash; index++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
