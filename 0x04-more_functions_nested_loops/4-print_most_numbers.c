946ad870a0a26fc54433ca64ea7212cb9ce8ca43
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-24 10:35:01 -0800

    Initial commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints digits except for 2 and 4
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
