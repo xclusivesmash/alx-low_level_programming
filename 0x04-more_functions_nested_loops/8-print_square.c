9375106bcc1656733485a214f2102c9104b44675
xclusivesmash <sbumatshiane916@gmail.com>
2023-02-24 11:31:11 -0800

    Initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using #
+ * @size: square size
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	/* Input check */
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
