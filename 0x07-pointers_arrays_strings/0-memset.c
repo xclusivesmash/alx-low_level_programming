b9edec1473db44ba740d273fb917ed763102d8f5
xclusivesmash <sbumatshiane916@gmail.com>
2023-03-06 15:49:59 -0800

    Fixed betty coding style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: pointer pointing to memory area
  * @b: byte to be filled in memory pointer to by s
  * @n: bytes to be filled
- * Return: pointer to memory area filled with b (s) 
+ * Return: pointer to memory area filled with b (s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
