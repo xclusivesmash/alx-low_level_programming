8464e65d588ee0eec11a21980c986f19b447492b
xclusivesmash <sbumatshiane916@gmail.com>
2023-03-06 16:23:13 -0800

    Initial commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string to be searched
+ * @c: character to be located in s
+ * Return: pointer to the first occurance of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	char *store;
+
+	while (s[i] != '\0')
+	{
+		store = &(s[i]);
+		if (*store == c)
+		{
+			return (store);
+		}
+		i = i + 1;
+	}
+	return (NULL);
+}
