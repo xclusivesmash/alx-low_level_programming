12d4bbc6747879228a52856f45d487c0ceba5a71
xclusivesmash <sbumatshiane916@gmail.com>
2023-03-15 07:03:17 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints programe name followed by \n
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (on success)
+ */
+int main(int __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
