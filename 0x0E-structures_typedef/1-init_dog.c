a2a88622d3de1de634d17094896f34726df321f1
xclusivesmash <sbumatshiane916@gmail.com>
2023-03-20 05:02:36 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog structure
+ * @d: structure to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
