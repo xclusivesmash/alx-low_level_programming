a0814e3bd25921ae9ef6bbad9a9dd49b74bc8604
xclusivesmash <sbumatshiane916@gmail.com>
2023-03-20 04:52:39 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a type to use for defining dogs
+ * @name: name of the dog
+ * @age: how old is the dog
+ * @owner: who owns the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
