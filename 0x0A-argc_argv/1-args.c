33f4345ff8eda2bd47c41be24fb000ea1e1b82ff
xclusivesmash <sbumatshiane916@gmail.com>
2023-06-09 04:31:33 -0700

    Initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints argc.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0 on success.
+ */
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
