8a5e3bf78922273174d4d1ef5369ac8b52f2652f
xclusivesmash <sbumatshiane916@gmail.com>
2023-06-12 06:50:59 -0700

    Bug fixed: NULL case for input strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *store;
 
 	/* ERROR HANDLING */
-	if (*s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (*s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
 
 	/* CODE SECTION */
