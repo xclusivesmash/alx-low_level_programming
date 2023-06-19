c7171c7488a8f91a134fa0e21368dd209d4284a4
xclusivesmash <sbumatshiane916@gmail.com>
2023-06-19 14:23:06 -0700

    Changed logic for correct output

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,82 @@
 #include "dog.h"
 
+int _strlen(char *s);
+
 /**
- * new_dog - creates an instance of dog_t.
- * @name: name of dog.
- * @age: dog's age.
- * @owner: owner of the pet.
- * Return: pointer to dog_t type.
- */
+* _strcpy - copies ones string contents to another
+* @dest:base string
+* @src: string from which to copy
+* Return: pointer to copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < _strlen(src); i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+/**
+* _strlen - computes length of string
+* @s: string
+* Return: len
+*/
+int _strlen(char *s)
+{
+	if (*s != '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
+/**
+* new_dog - creates a new dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: pointer to struct dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *store;
+	dog_t *d_new;
+	char *name_copy;
+	char *owner_copy;
 
-	store = malloc(sizeof(dog_t));
-	if (store == NULL)
+	d_new = malloc(sizeof(dog_t));
+
+	/*check functionality*/
+	if (d_new == NULL)
 		return (NULL);
 
-	store->name = name;
-	store->age = age;
-	store->owner = owner;
-	return (store);
+	d_new->age = age;
+
+	if (name == NULL)
+		d_new->name = NULL;
+	else
+	{
+		name_copy = malloc(_strlen(name) + 1);
+		if (name_copy == NULL)
+		{
+			free(name_copy);
+			free(d_new);
+			return (NULL);
+		}
+		d_new->name = _strcpy(name_copy, name);
+	}
+
+	if (owner == NULL)
+		d_new->owner = NULL;
+	else
+	{
+		owner_copy = malloc(_strlen(owner) + 1);
+		if (owner_copy == NULL)
+		{
+			free(owner_copy);
+			free(d_new);
+			return (NULL);
+		}
+		d_new->owner = _strcpy(owner_copy, owner);
+	}
+
+	return (d_new);
 }
