ff7cecb588413707faf16977858a61d96cc3cd13
xclusivesmash <sbumatshiane916@gmail.com>
2023-07-04 09:55:10 -0700

    Fixed checker error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,9 +33,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			store->next = temp->next;
 			temp->next = store;
-			break;
+			return (store);
 		}
 		i = i + 1;
 	}
-	return (store);
+	return (NULL);
 }
