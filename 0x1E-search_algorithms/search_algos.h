b04ecedbe5b1f37ef95823bce90d3ba50044602c
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-29 12:40:41 -0700

    Added header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Include libraries to use */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Structure definitins */
+
+/* function declarations */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
